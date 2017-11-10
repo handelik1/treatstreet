52856b5dc4b4954b1936628e0e362b3a525a6b55
handelik1 <handelik1@gmail.com>
2017-11-09 22:59:43 -0500

    Add files via upload

--- treatstreet/admin.php
+++ treatstreet/admin.php
@@ -8,7 +8,7 @@ require('connect.php');
 require('header.html');
 
 $out = '';
-$out .= '<body>';
+$out .= '<body class = "admin-body">';
 $out .= 	'<div class="container">';
 $out .= 		'<div class="row">';
 $out .=		    	'<div class="col-md-12">';
@@ -22,6 +22,36 @@ $out .=		    	'<div class="col-md-12">';
 $out .=				'</div>';
 $out .=     	'</div>';
 
+	$getTotal = mysqli_query($con, "select SUM(total) from orders");
+	$total = mysqli_fetch_row($getTotal);
+
+$out .= 		'<div class="row">';
+
+$out .=		    	'<div class="col-md-6">';
+
+$out .=					'<div class="admin-card">';
+$out .=						'<img class = "profit-img" src="src/img/profit.png" alt="Profit">';
+$out .=							'<div class="profit-wrapper">';
+$out .=						    	'<p class = "profit"><span>Earnings</span><br>$'.$total[0].'</p>';
+$out .=						 	'</div>';
+$out .=					'</div>';
+
+$out .=				'</div>';
+
+	$getUsers = mysqli_query($con, "select count(username) from users");
+	$users = mysqli_fetch_row($getUsers);
+
+$out .=		    	'<div class="col-md-6">';
+
+$out .=					'<div class="admin-card">';
+$out .=						'<img class = "profit-img" src="src/img/users.png" alt="Profit">';
+$out .=							'<div class="profit-wrapper">';
+$out .=						    	'<p class = "profit"><span>Customers</span><br>'.$users[0].'</p>';
+$out .=						 	'</div>';
+$out .=					'</div>';
+$out .=				'</div>';
+
+$out .=     	'</div>';
 
 
 $out .= 	'</div>';
